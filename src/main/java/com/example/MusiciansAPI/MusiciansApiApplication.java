8079827db3cec20b2ca63e1c3d23b9a0740ad6f8
shulginmusic <efim@shulginmusic.com>
2021-08-06 12:50:53 -0700

    Init commit

---
+++ src/main/java/com/example/MusiciansAPI/MusiciansApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.MusiciansAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusiciansApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusiciansApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MusiciansAPI/MusiciansApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MusiciansAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusiciansApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
