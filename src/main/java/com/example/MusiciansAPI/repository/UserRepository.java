51dddcbd53842bc390e956c23cbdbf7ef3a13e99
shulginmusic <efim@shulginmusic.com>
2021-08-08 21:41:58 -0700

    User Principal + DTO

--- src/main/java/com/example/MusiciansAPI/repository/UserRepository.java
+++ src/main/java/com/example/MusiciansAPI/repository/UserRepository.java
@@ -1,2 +1,23 @@
-package com.example.MusiciansAPI.repository;public class UserRepository {
+package com.example.MusiciansAPI.repository;
+
+import com.example.MusiciansAPI.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findByUsernameOrEmail(String username, String email);
+
+    Optional<User> findById(Long userId); //TODO This returned a list before ...
+
+    Optional<User> findByUsername(String username);
+
+    Boolean existsByUsername(String username);
+
+    Boolean existsByEmail(String email);
 }
+
--- src/main/java/com/example/MusiciansAPI/security/UserPrincipalDTO.java
+++ src/main/java/com/example/MusiciansAPI/security/UserPrincipalDTO.java
@@ -1,2 +1,107 @@
-package com.example.MusiciansAPI.security;public class UserPrincipal {
+package com.example.MusiciansAPI.security;
+
+import com.example.MusiciansAPI.model.User;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+/**
+ * custom UserDetails class called UserPrincipal.
+ * This is the class whose instances will be returned
+ * from our custom UserDetailsService.
+ */
+
+@Getter
+public class UserPrincipalDTO implements UserDetails {
+    private Long id;
+    private String name;
+    private String username;
+
+    @JsonIgnore
+    private String email;
+
+    @JsonIgnore
+    private String password;
+
+    private Collection<? extends GrantedAuthority> authorities;
+
+    public UserPrincipalDTO(Long id, String name, String username, String email,
+                            String password, Collection<? extends GrantedAuthority> authorities) {
+        this.id = id;
+        this.name = name;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.authorities = authorities;
+    }
+
+    public static UserPrincipalDTO create(User user) {
+        List<GrantedAuthority> authorities = user.getRoles().stream().map(role ->
+                new SimpleGrantedAuthority(role.getName().name()))
+                .collect(Collectors.toList());
+
+        return new UserPrincipalDTO(
+                user.getId(),
+                user.getName(),
+                user.getUsername(),
+                user.getEmail(),
+                user.getPassword(),
+                authorities
+        );
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserPrincipalDTO that = (UserPrincipalDTO) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
