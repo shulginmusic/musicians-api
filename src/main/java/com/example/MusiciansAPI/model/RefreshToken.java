35b80ffa458288fcb91086be92a094664527635c
shulginmusic <efim@shulginmusic.com>
2021-08-20 21:44:04 -0700

    Token Refresh Exception + Model

---
+++ src/main/java/com/example/MusiciansAPI/model/RefreshToken.java
@@ -0,0 +1,31 @@
+package com.example.MusiciansAPI.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.Instant;
+
+@NoArgsConstructor
+@Getter
+@Setter
+
+@Entity(name = "refresh_token")
+public class RefreshToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+
+    @Column(nullable = false, unique = true)
+    private String token;
+
+    @Column(nullable = false)
+    private Instant expiryDate;
+
+
+}
