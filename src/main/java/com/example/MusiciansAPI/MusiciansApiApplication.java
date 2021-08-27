fbc83700aa5c4f0b0544beaba94c880c18bf3388
shulginmusic <efim@shulginmusic.com>
2021-08-27 13:23:34 -0700

    MySQL implementation

--- src/main/java/com/example/MusiciansAPI/MusiciansApiApplication.java
+++ src/main/java/com/example/MusiciansAPI/MusiciansApiApplication.java
@@ -3,10 +3,15 @@ package com.example.MusiciansAPI;
 import com.example.MusiciansAPI.model.Role;
 import com.example.MusiciansAPI.repository.RoleRepository;
 import com.example.MusiciansAPI.service.RoleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.EventListener;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class MusiciansApiApplication {
@@ -18,6 +23,8 @@ public class MusiciansApiApplication {
 	@Bean
 	CommandLineRunner runner(RoleRepository roleRepository) {
 		return args -> {
+			//Delete roles from repo if there are any left from previous run (Thank you Ryan Desmond)
+			roleRepository.deleteAll();
 			//Save roles at runtime
 			Role userRole = new Role(Role.RoleName.ROLE_USER);
 			Role adminRole = new Role(Role.RoleName.ROLE_ADMIN);
